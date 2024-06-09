97641d990701266e086270291631da3b571bac38
Rohit <croods660@gmail.com>
2024-06-09 16:47:19 +0530

    made a simple blog website

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
+from django.http import HttpResponseRedirect
+from blog.forms import CommentForm
 
 # Create your views here.
 
@@ -17,15 +19,28 @@ def blog_category(request, category):
         "category": category,
         "posts": posts,
     }
-
-    return render(request, "blog", context)
+    
+    return render(request, "blog/category.html", context)
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm,
     }
 
     return render(request, "blog/detail.html", context)
